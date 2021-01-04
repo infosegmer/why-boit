65a6b4e09208374a29538aab2651b527375926b7
M0kser <73760464+M0kser@users.noreply.github.com>
2021-01-04 19:09:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'TOKEN': 'Nzg2OTcxMzA2NDc4NDAzNjA0.X9OKJA.oQdfncecphatmQl10KJzx_EU4ww',
+    'TOKEN': 'Nzg2OTcxMzA2NDc4NDAzNjA0.X9OKJA.9mj1jP30kO0XTMUeCP_MdO4VKXs',
     'NAME BOT': 'Cloudon',
     'ID': 786971306478403604,
     'PREFIX': '+'
